43945d9780819419915bb4af47f201e00725122e
Toshio Ito <debug.ito@gmail.com>
2017-09-30 17:21:13 +0900

    temporarily comment out Binder and GTraversal
    to pass compile.

--- src/Data/Greskell/Binder.hs
+++ src/Data/Greskell/Binder.hs
@@ -7,51 +7,51 @@
 -- 
 module Data.Greskell.Binder
        ( -- * Types
-         Binder,
-         Binding,
-         -- * Actions
-         newBind,
-         -- * Runners
-         runBinder
+--          Binder,
+--          Binding,
+--          -- * Actions
+--          newBind,
+--          -- * Runners
+--          runBinder
        ) where
 
-import Control.Monad.Trans.State (State)
-import qualified Control.Monad.Trans.State as State
-import Data.Aeson (Value, ToJSON(toJSON), Object)
-import Data.Monoid ((<>))
-import qualified Data.HashMap.Strict as HM
-import Data.Text (Text, pack)
-
-import Data.Greskell.Greskell
-  ( Greskell, placeHolder, toPlaceHolderVariable,
-    PlaceHolderIndex
-  )
-
--- | A Monad that manages a 'Binding' in its context.
-newtype Binder a = Binder { unBinder :: State (PlaceHolderIndex, [Value]) a }
-                   deriving (Functor, Applicative, Monad)
-
--- | Binding between Gremlin variable names and JSON values.
-type Binding = Object
-
--- | Create a new Gremlin variable bound to the given value.
-newBind :: ToJSON v
-        => v -- ^ bound value
-        -> Binder Greskell -- ^ variable
-newBind val = Binder $ do
-  (next_index, values) <- State.get
-  State.put (succ next_index, values ++ [toJSON val])
-  return $ placeHolder next_index
-
--- | Execute the given 'Binder' monad to obtain 'Binding'.
-runBinder :: Binder a -> (a, Binding)
-runBinder binder = (ret, binding)
-  where
-    (ret, (_, values)) = State.runState (unBinder binder) (0, [])
-    binding = HM.fromList $ zip (map toPlaceHolderVariable [0 ..]) $ values
-
--- seqGremlin :: [Binder Text] -> Binder Text
--- seqGremlin = fmap seqSentences . sequence
---   where
---     seqSentences = T.intercalate "; "
+-- import Control.Monad.Trans.State (State)
+-- import qualified Control.Monad.Trans.State as State
+-- import Data.Aeson (Value, ToJSON(toJSON), Object)
+-- import Data.Monoid ((<>))
+-- import qualified Data.HashMap.Strict as HM
+-- import Data.Text (Text, pack)
 -- 
+-- import Data.Greskell.Greskell
+--   ( Greskell, placeHolder, toPlaceHolderVariable,
+--     PlaceHolderIndex
+--   )
+-- 
+-- -- | A Monad that manages a 'Binding' in its context.
+-- newtype Binder a = Binder { unBinder :: State (PlaceHolderIndex, [Value]) a }
+--                    deriving (Functor, Applicative, Monad)
+-- 
+-- -- | Binding between Gremlin variable names and JSON values.
+-- type Binding = Object
+-- 
+-- -- | Create a new Gremlin variable bound to the given value.
+-- newBind :: ToJSON v
+--         => v -- ^ bound value
+--         -> Binder Greskell -- ^ variable
+-- newBind val = Binder $ do
+--   (next_index, values) <- State.get
+--   State.put (succ next_index, values ++ [toJSON val])
+--   return $ placeHolder next_index
+-- 
+-- -- | Execute the given 'Binder' monad to obtain 'Binding'.
+-- runBinder :: Binder a -> (a, Binding)
+-- runBinder binder = (ret, binding)
+--   where
+--     (ret, (_, values)) = State.runState (unBinder binder) (0, [])
+--     binding = HM.fromList $ zip (map toPlaceHolderVariable [0 ..]) $ values
+-- 
+-- -- seqGremlin :: [Binder Text] -> Binder Text
+-- -- seqGremlin = fmap seqSentences . sequence
+-- --   where
+-- --     seqSentences = T.intercalate "; "
+-- -- 
--- test/Data/Greskell/GTraversalSpec.hs
+++ test/Data/Greskell/GTraversalSpec.hs
@@ -1,113 +1,116 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Greskell.GTraversalSpec (main,spec) where
 
-import Control.Category ((>>>), (<<<))
-import Data.Either (isRight)
-import Data.Function ((&))
-import Language.Haskell.Interpreter
-  ( loadModules, OptionVal((:=)), set, searchPath,
-    setTopLevelModules, runInterpreter, InterpreterError,
-    typeOf
-  )
-import System.IO (stderr, hPutStrLn)
+-- import Control.Category ((>>>), (<<<))
+-- import Data.Either (isRight)
+-- import Data.Function ((&))
+-- import Language.Haskell.Interpreter
+--   ( loadModules, OptionVal((:=)), set, searchPath,
+--     setTopLevelModules, runInterpreter, InterpreterError,
+--     typeOf
+--   )
+-- import System.IO (stderr, hPutStrLn)
+
 import Test.Hspec
 
-import Data.Greskell.Greskell (runGreskell', raw, rawS)
-import Data.Greskell.GTraversal
-  ( source, vertices, (&.), ($.),
-    gHas', gOut, gRange, gValues, gNot, gIn
-  )
+-- 
+-- import Data.Greskell.Greskell (runGreskell', raw, rawS)
+-- import Data.Greskell.GTraversal
+--   ( source, vertices, (&.), ($.),
+--     gHas', gOut, gRange, gValues, gNot, gIn
+--   )
 
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-  spec_StepType_classes
-  spec_GTraversalSource
-  spec_compose_steps
+spec = describe "todo" $ specify "todo" $ True `shouldBe` False
+-- spec = do
+--   spec_StepType_classes
+--   spec_GTraversalSource
+--   spec_compose_steps
 
-spec_StepType_classes :: Spec
-spec_StepType_classes = do
-  describe "Split typeclass" $ do
-    let c = checkSplitCompatible
-    c "Filter" "Filter" True
-    c "Filter" "Transform" True
-    c "Filter" "SideEffect" True
-    c "Transform" "Filter" True
-    c "Transform" "Transform" True
-    c "Transform" "SideEffect" True
-    c "SideEffect" "Filter" False
-    c "SideEffect" "Transform" False
-    c "SideEffect" "SideEffect" True
-  describe "Lift typeclass" $ do
-    let c = checkLiftCompatible
-    c "Filter" "Filter" True
-    c "Filter" "Transform" True
-    c "Filter" "SideEffect" True
-    c "Transform" "Filter" False
-    c "Transform" "Transform" True
-    c "Transform" "SideEffect" True
-    c "SideEffect" "Filter" False
-    c "SideEffect" "Transform" False
-    c "SideEffect" "SideEffect" True
-  
-toErrString :: Either InterpreterError a -> Either String a
-toErrString (Right a) = Right a
-toErrString (Left e) = Left $ show e
-
-checkStepTypeRelation :: (String -> String -> String) -> String -> String -> Bool -> Spec
-checkStepTypeRelation makeCode child parent expect_ok = specify label $ doCheck
-  where
-    label = child ++ " -> " ++ parent
-    doCheck = do
-      result <- fmap toErrString $ runInterpreter compiledParent
-      -- hPutStrLn stderr ("## " ++ label ++ ": " ++ show result)
-      isRight result `shouldBe` expect_ok
-    compiledParent = do
-      set [searchPath := ["src"]]
-      loadModules ["src/Data/Greskell/GTraversal.hs"]
-      setTopLevelModules ["Data.Greskell.GTraversal"]
-      typeOf $ makeCode child parent
-
-checkSplitCompatible :: String -> String -> Bool -> Spec
-checkSplitCompatible = checkStepTypeRelation makeCode
-  where
-    makeCode child parent =
-      "let f :: Step " ++ child ++ " s s -> Step " ++ parent ++ " s s; "
-      ++ "f = gFilter; "
-      ++ "child :: Step " ++ child ++ " s s; "
-      ++ "child = undefined; "
-      ++ "in f child"
-
-checkLiftCompatible :: String -> String -> Bool -> Spec
-checkLiftCompatible = checkStepTypeRelation makeCode
-  where
-    makeCode child parent =
-      "let f :: Step " ++ child ++ " s e -> Step " ++ parent ++ " s e; "
-      ++ "f = liftType; "
-      ++ "in f"
-
-spec_GTraversalSource :: Spec
-spec_GTraversalSource = describe "GTraversalSource" $ do
-  specify "g.V()" $ do
-    (runGreskell' $ vertices [] $ source "g") `shouldBe` ("g.V()")
-  specify "g.V(1,2,3)" $ do
-    let ids = [1,2,3] :: [Int]
-    (runGreskell' $ vertices (map rawS ids) $ source "g") `shouldBe` ("g.V(1,2,3)")
-
-spec_compose_steps :: Spec
-spec_compose_steps = describe "DSL to compose steps" $ do
-  specify "(&) and (&.)" $ do
-    let gt = source "g" & vertices [] &. gHas' "x" (raw "100") &. gOut [] &. gRange (raw "0") (raw "100")
-    runGreskell' gt `shouldBe` "g.V().has(\"x\",100).out().range(0,100)"
-  specify "(&) and (&.) and (>>>)" $ do
-    let gt = source "g" & vertices [raw "200"] &. (gOut [] >>> gOut ["friends_to"] >>> gValues ["name"])
-    runGreskell' gt `shouldBe` "g.V(200).out().out(\"friends_to\").values(\"name\")"
-  specify "($) and ($.)" $ do
-    let gt = gRange (raw "20") (raw "30") $. gNot (gOut ["friends_to"]) $. vertices [] $ source "g"
-    runGreskell' gt `shouldBe` "g.V().not(__.out(\"friends_to\")).range(20,30)"
-  specify "($) and ($.) and (<<<)" $ do
-    let gt = gHas' "name" "hoge" <<< gIn ["foo", "bar"] <<< gIn [] $. vertices [] $ source "g"
-    runGreskell' gt `shouldBe` "g.V().in().in(\"foo\",\"bar\").has(\"name\",\"hoge\")"
+-- spec_StepType_classes :: Spec
+-- spec_StepType_classes = do
+--   describe "Split typeclass" $ do
+--     let c = checkSplitCompatible
+--     c "Filter" "Filter" True
+--     c "Filter" "Transform" True
+--     c "Filter" "SideEffect" True
+--     c "Transform" "Filter" True
+--     c "Transform" "Transform" True
+--     c "Transform" "SideEffect" True
+--     c "SideEffect" "Filter" False
+--     c "SideEffect" "Transform" False
+--     c "SideEffect" "SideEffect" True
+--   describe "Lift typeclass" $ do
+--     let c = checkLiftCompatible
+--     c "Filter" "Filter" True
+--     c "Filter" "Transform" True
+--     c "Filter" "SideEffect" True
+--     c "Transform" "Filter" False
+--     c "Transform" "Transform" True
+--     c "Transform" "SideEffect" True
+--     c "SideEffect" "Filter" False
+--     c "SideEffect" "Transform" False
+--     c "SideEffect" "SideEffect" True
+--   
+-- toErrString :: Either InterpreterError a -> Either String a
+-- toErrString (Right a) = Right a
+-- toErrString (Left e) = Left $ show e
+-- 
+-- checkStepTypeRelation :: (String -> String -> String) -> String -> String -> Bool -> Spec
+-- checkStepTypeRelation makeCode child parent expect_ok = specify label $ doCheck
+--   where
+--     label = child ++ " -> " ++ parent
+--     doCheck = do
+--       result <- fmap toErrString $ runInterpreter compiledParent
+--       -- hPutStrLn stderr ("## " ++ label ++ ": " ++ show result)
+--       isRight result `shouldBe` expect_ok
+--     compiledParent = do
+--       set [searchPath := ["src"]]
+--       loadModules ["src/Data/Greskell/GTraversal.hs"]
+--       setTopLevelModules ["Data.Greskell.GTraversal"]
+--       typeOf $ makeCode child parent
+-- 
+-- checkSplitCompatible :: String -> String -> Bool -> Spec
+-- checkSplitCompatible = checkStepTypeRelation makeCode
+--   where
+--     makeCode child parent =
+--       "let f :: Step " ++ child ++ " s s -> Step " ++ parent ++ " s s; "
+--       ++ "f = gFilter; "
+--       ++ "child :: Step " ++ child ++ " s s; "
+--       ++ "child = undefined; "
+--       ++ "in f child"
+-- 
+-- checkLiftCompatible :: String -> String -> Bool -> Spec
+-- checkLiftCompatible = checkStepTypeRelation makeCode
+--   where
+--     makeCode child parent =
+--       "let f :: Step " ++ child ++ " s e -> Step " ++ parent ++ " s e; "
+--       ++ "f = liftType; "
+--       ++ "in f"
+-- 
+-- spec_GTraversalSource :: Spec
+-- spec_GTraversalSource = describe "GTraversalSource" $ do
+--   specify "g.V()" $ do
+--     (runGreskell' $ vertices [] $ source "g") `shouldBe` ("g.V()")
+--   specify "g.V(1,2,3)" $ do
+--     let ids = [1,2,3] :: [Int]
+--     (runGreskell' $ vertices (map rawS ids) $ source "g") `shouldBe` ("g.V(1,2,3)")
+-- 
+-- spec_compose_steps :: Spec
+-- spec_compose_steps = describe "DSL to compose steps" $ do
+--   specify "(&) and (&.)" $ do
+--     let gt = source "g" & vertices [] &. gHas' "x" (raw "100") &. gOut [] &. gRange (raw "0") (raw "100")
+--     runGreskell' gt `shouldBe` "g.V().has(\"x\",100).out().range(0,100)"
+--   specify "(&) and (&.) and (>>>)" $ do
+--     let gt = source "g" & vertices [raw "200"] &. (gOut [] >>> gOut ["friends_to"] >>> gValues ["name"])
+--     runGreskell' gt `shouldBe` "g.V(200).out().out(\"friends_to\").values(\"name\")"
+--   specify "($) and ($.)" $ do
+--     let gt = gRange (raw "20") (raw "30") $. gNot (gOut ["friends_to"]) $. vertices [] $ source "g"
+--     runGreskell' gt `shouldBe` "g.V().not(__.out(\"friends_to\")).range(20,30)"
+--   specify "($) and ($.) and (<<<)" $ do
+--     let gt = gHas' "name" "hoge" <<< gIn ["foo", "bar"] <<< gIn [] $. vertices [] $ source "g"
+--     runGreskell' gt `shouldBe` "g.V().in().in(\"foo\",\"bar\").has(\"name\",\"hoge\")"
