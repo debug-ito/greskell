ea5f60c343dc2f2885d47a098d19009f00441920
Toshio Ito <debug.ito@gmail.com>
2017-09-30 18:16:28 +0900

    Fix GreskellSpec.

--- test/Data/Greskell/GreskellSpec.hs
+++ test/Data/Greskell/GreskellSpec.hs
@@ -8,9 +8,10 @@ import Test.Hspec
 import Test.QuickCheck (property, Arbitrary(..))
 
 import Data.Greskell.Greskell
-  ( raw, rawS, runGreskell, string,
-    placeHolder, toPlaceHolderVariable,
-    funCall, methodCall
+  ( unsafeGreskell, runGreskell, string,
+    unsafePlaceHolder, toPlaceHolderVariable,
+    unsafeFunCall, unsafeMethodCall,
+    Greskell
   )
 
 -- TODO: move this into a single support module.
@@ -22,36 +23,51 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "raw" $ it "should be just a raw script text" $ property $ \t ->
-    (runGreskell $ raw t) `shouldBe` t
-  describe "rawS" $ do
+  describe "unsafeGreskell" $ it "should be just a raw script text" $ property $ \t ->
+    (runGreskell $ unsafeGreskell t) `shouldBe` t
+  describe "Num" $ do
     specify "integer" $ do
-      let x :: Int
-          x = 123
-      (runGreskell $ rawS x) `shouldBe` "123"
+      let x = 100 :: Greskell Int
+      runGreskell x `shouldBe` "123"
     specify "negative integer" $ do
-      let x :: Int
-          x = -56
-      (runGreskell $ rawS x) `shouldBe` "-56"
-    specify "floating" $ do
-      let x :: Double
-          x = 92.12
-      (runGreskell $ rawS x) `shouldBe` "92.12"
+      let x = -56 :: Greskell Int
+      runGreskell x `shouldBe` "-(56)"
+    specify "operations" $ do
+      let x = (30 + 15 * 20 - 10) :: Greskell Int
+      runGreskell x `shouldBe` "((30)+((15)*(20)))-(10)"
+    specify "abs, signum" $ do
+      let x = (signum $ abs (-100)) :: Greskell Int
+      runGreskell x  `shouldBe` "java.lang.Long.signum(java.lang.Math.abs(-(100)))"
+  describe "Fractional" $ do
+    specify "floating point literal" $ do
+      let x = 92.12 :: Greskell Double
+      (runGreskell x) `shouldBe` "(2303.0/25)"
+    specify "operations" $ do
+      let x = (100.5 * recip 30 / 20.2) :: Greskell Double
+      runGreskell x `shouldBe` "((201.0/2)*(0.1/(30.0/1)))/((101.0/5))"
+  describe "Monoid" $ do
+    specify "mempty" $ do
+      let got = mempty :: Greskell Text
+      runGreskell got `shouldBe` "\"\""
+    specify "mappend" $ do
+      let got = (mappend "foo" "bar") :: Greskell Text
+      runGreskell got `shouldBe` "(\"foo\")+(\"bar\")"
   describe "placeHolder" $ it "should create a placeholder variable" $ property $ \i ->
-    (runGreskell $ placeHolder i) `shouldBe` toPlaceHolderVariable i
-  describe "gLateral and fromString" $ do
-    specify "empty" $ checkLiteral "" "\"\""
-    specify "words" $ checkLiteral "hoge foo bar"  "\"hoge foo bar\""
-    specify "escaped" $ checkLiteral "foo 'aaa \n \t \\ \"bar\"" "\"foo 'aaa \\n \\t \\\\ \\\"bar\\\"\""
-  describe "funCall" $ do
+    (runGreskell $ unsafePlaceHolder i) `shouldBe` toPlaceHolderVariable i
+  describe "string and fromString" $ do
+    specify "empty" $ checkStringLiteral "" "\"\""
+    specify "words" $ checkStringLiteral "hoge foo bar"  "\"hoge foo bar\""
+    specify "escaped" $ checkStringLiteral "foo 'aaa \n \t \\ \"bar\"" "\"foo 'aaa \\n \\t \\\\ \\\"bar\\\"\""
+  describe "unsafeFunCall" $ do
     it "should make function call" $ do
-      (runGreskell $ funCall "fun" ["foo", raw "bar"]) `shouldBe` "fun(\"foo\",bar)"
-  describe "methodCall" $ do
+      (runGreskell $ unsafeFunCall "fun" ["foo", "bar"]) `shouldBe` "fun(foo,bar)"
+  describe "unsafeMethodCall" $ do
     it "should make method call" $ do
-      (runGreskell $ methodCall "meth" [raw "hoge", raw "foo", "bar"]) `shouldBe` ".meth(hoge,foo,\"bar\")"
+      (runGreskell $ unsafeMethodCall "meth" ["hoge", "foo", "bar"]) `shouldBe` ".meth(hoge,foo,bar)"
 
 
-checkLiteral :: String -> Text -> Expectation
-checkLiteral input expected = do
-  (runGreskell $ fromString input) `shouldBe` expected
+checkStringLiteral :: String -> Text -> Expectation
+checkStringLiteral input expected = do
+  let input' = fromString input :: Greskell Text
+  (runGreskell $ input') `shouldBe` expected
   (runGreskell $ string $ pack input) `shouldBe` expected
