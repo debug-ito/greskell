b4bc0f2ae5fd15b714cdb37017f836203db8eef0
Toshio Ito <debug.ito@gmail.com>
2017-10-09 21:29:07 +0900

    Add Predicate and P.

--- src/Data/Greskell/Gremlin.hs
+++ src/Data/Greskell/Gremlin.hs
@@ -6,7 +6,10 @@
 --
 -- 
 module Data.Greskell.Gremlin
-       ( -- * Comparator
+       ( -- * Predicate
+         Predicate(..),
+         P,
+         -- * Comparator
          Comparator(..),
          -- ** org.apache.tinkerpop.gremlin.process.traversal.Order
          Order,
@@ -22,17 +25,41 @@ import Data.Aeson (Value)
 import Data.Monoid ((<>))
 import Data.Greskell.Greskell
   ( Greskell, unsafeGreskellLazy,
-    toGremlinLazy
+    toGremlin, toGremlinLazy, unsafeMethodCall, unsafeFunCall
   )
+
+-- | @java.util.function.Predicate@ interface.
+class Predicate p where
+  type PredicateArg p
+  pAnd :: Greskell p -> Greskell p -> Greskell p
+  pAnd p1 p2 = unsafeMethodCall p1 "and" [toGremlin p2]
+  pOr :: Greskell p -> Greskell p -> Greskell p
+  pOr o1 o2 = unsafeMethodCall o1 "or" [toGremlin o2]
+  pTest :: Greskell p -> Greskell (PredicateArg p) -> Greskell Bool
+  pTest p arg = unsafeMethodCall p "test" [toGremlin arg]
+  pNegate :: Greskell p -> Greskell p
+  pNegate p = unsafeMethodCall p "negate" []
+
+-- | @org.apache.tinkerpop.gremlin.process.traversal.P@ class.
+--
+-- @P a@ keeps data of type @a@ and compare it with data of type @a@
+-- given as the Predicate argument.
+data P a
+
+instance Predicate (P a) where
+  type PredicateArg (P a) = a
+
+-- | @P.eq@ static method.
+pEq :: Greskell a -> Greskell (P a)
+pEq arg = unsafeFunCall "eq" [toGremlin arg]
+
+-- TODO: other P methods.
 
 -- | @java.util.Comparator@ interface.
 class Comparator c where
   type CompareArg c
   cCompare :: Greskell c -> Greskell (CompareArg c) -> Greskell (CompareArg c) -> Greskell Int
-  cCompare cmp a b = unsafeGreskellLazy
-                     ( "(" <> toGremlinLazy cmp <> ").compare("
-                       <> toGremlinLazy a <> "," <> toGremlinLazy b <> ")"
-                     )
+  cCompare cmp a b = unsafeMethodCall cmp "compare" $ map toGremlin [a, b]
                      
 -- | org.apache.tinkerpop.gremlin.process.traversal.Order enum.
 data Order a
