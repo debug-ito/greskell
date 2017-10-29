1427f4ea8623b34dc4f9a08d5b682450b2dabceb
Toshio Ito <debug.ito@gmail.com>
2017-10-29 21:27:13 +0900

    Add GremlinSpec.

---
+++ test/Data/Greskell/GremlinSpec.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Data.Greskell.GremlinSpec (main,spec) where
+
+import Test.Hspec
+
+import Data.Greskell.Gremlin
+  ( pBetween, pAnd, pOr, pNegate, pWithin, pGte, pTest
+  )
+import Data.Greskell.Greskell (toGremlin, Greskell)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "P and Predicate" $ do
+    specify "P and Predicate methods" $ do
+      let expr = (pBetween (10 :: Greskell Int) 50 `pAnd` ((pNegate (pWithin [5, 15, 25, 35])) `pOr` pGte 20)) `pTest` 50
+      toGremlin expr `shouldBe`
+        "((between(10,50)).and(((within(5,15,25,35)).negate()).or(gte(20)))).test(50)"
