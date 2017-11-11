731997e3018ec42b65d3938a8c93ac804b482fe9
Toshio Ito <debug.ito@gmail.com>
2017-11-11 18:05:52 +0900

    Add GraphSON module.

---
+++ src/Data/Greskell/GraphSON.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Module: Data.Greskell.GraphSON
+-- Description: Encoding and decoding GraphSON
+-- Maintainer: Toshio Ito <debug.ito@gmail.com>
+--
+-- 
+module Data.Greskell.GraphSON
+       ( GraphSON(..),
+         nonTypedGraphSON,
+         typedGraphSON
+       ) where
+
+import Control.Applicative ((<$>), (<*>))
+import Data.Aeson (ToJSON(toJSON), FromJSON(parseJSON), object, (.=), Value(Object), (.:?))
+import Data.Aeson.Types (Parser)
+import Data.Foldable (Foldable(foldr))
+import Data.Text (Text)
+import Data.Traversable (Traversable(traverse))
+
+-- | Wrapper of \"typed JSON object\" introduced in GraphSON version
+-- 2. See http://tinkerpop.apache.org/docs/current/dev/io/#graphson
+data GraphSON v =
+  GraphSON
+  { gsonType :: Maybe Text,
+    -- ^ Type ID, corresponding to @\@type@ field.
+    gsonValue :: v
+    -- ^ Value, correspoding to @\@value@ field.
+  }
+  deriving (Show,Eq,Ord)
+
+instance Functor GraphSON where
+  fmap f gs = gs { gsonValue = f $ gsonValue gs }
+
+instance Foldable GraphSON where
+  foldr f start gs = f (gsonValue gs) start
+
+instance Traversable GraphSON where
+  traverse f gs = fmap (\v -> gs { gsonValue = v }) $ f $ gsonValue gs
+
+-- | Create a 'GraphSON' without 'gsonType'.
+nonTypedGraphSON :: v -> GraphSON v
+nonTypedGraphSON = GraphSON Nothing
+
+-- | Create a 'GraphSON' with the given 'Text' as 'gsonType'.
+typedGraphSON :: Text -> v -> GraphSON v
+typedGraphSON t = GraphSON (Just t)
+
+-- | If 'gsonType' is 'Just', the 'GraphSON' is encoded as a typed
+-- JSON object. If 'gsonType' is 'Nothing', the 'gsonValue' is
+-- directly encoded.
+instance ToJSON v => ToJSON (GraphSON v) where
+  toJSON gson = case gsonType gson of
+    Nothing -> toJSON $ gsonValue gson
+    Just t -> object [ "@type" .= t,
+                       "@value" .= gsonValue gson
+                     ]
+
+-- | If the given 'Value' is a typed JSON object, 'gsonType' field of
+-- the result is 'Just'. Otherwise, the given 'Value' is directly
+-- parsed into 'gsonValue', and 'gsonType' is 'Nothing'.
+instance FromJSON v => FromJSON (GraphSON v) where
+  parseJSON v@(Object o) = do
+    if length o /= 2
+      then parseDirect v
+      else do
+      mtype <- o .:? "@type"
+      mvalue <- o .:? "@value"
+      maybe (parseDirect v) return $ typedGraphSON <$> mtype <*> mvalue
+  parseJSON v = parseDirect v
+    
+parseDirect :: FromJSON v => Value -> Parser (GraphSON v)
+parseDirect v = GraphSON Nothing <$> parseJSON v
+
