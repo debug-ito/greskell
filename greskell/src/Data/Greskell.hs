c8aa420d43ef759fb3774dc2c5556d4f54e46205
Toshio Ito <debug.ito@gmail.com>
2018-01-14 10:10:31 +0900

    Data.Greskell: re-export modules, including ones from -core.

--- greskell/src/Data/Greskell.hs
+++ greskell/src/Data/Greskell.hs
@@ -1,8 +1,21 @@
 -- |
 -- Module: Data.Greskell
--- Description:
+-- Description: Haskell binding for Gremlin graph query language
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module Data.Greskell
-       () where
+       (
+         module Data.Greskell.Greskell,
+         module Data.Greskell.Binder,
+         module Data.Greskell.GTraversal,
+         module Data.Greskell.Gremlin,
+         module Data.Greskell.Graph,
+         module Data.Greskell.GraphSON
+       ) where
 
+import Data.Greskell.Greskell
+import Data.Greskell.Binder
+import Data.Greskell.GTraversal
+import Data.Greskell.Gremlin
+import Data.Greskell.Graph
+import Data.Greskell.GraphSON
