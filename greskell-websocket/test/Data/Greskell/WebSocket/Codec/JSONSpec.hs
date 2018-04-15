2db035d8ce7462a35919d3d6a21b0e222cf72549
Toshio Ito <debug.ito@gmail.com>
2018-04-15 22:27:15 +0900

    JSONSpec: add test spec for simple standard responses.

--- greskell-websocket/test/Data/Greskell/WebSocket/Codec/JSONSpec.hs
+++ greskell-websocket/test/Data/Greskell/WebSocket/Codec/JSONSpec.hs
@@ -3,8 +3,10 @@ module Data.Greskell.WebSocket.Codec.JSONSpec (main,spec) where
 
 import Control.Applicative ((<$>))
 import Control.Monad (forM_)
-import Data.Aeson (Value(Null))
+import Data.Aeson (Value(Null), (.=))
+import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as BSL
+import Data.Greskell.GraphSON (GraphSON, nonTypedGraphSON, typedGraphSON, typedGraphSON')
 import Data.Maybe (fromJust)
 import Data.Monoid (mempty)
 import qualified Data.UUID as UUID
@@ -23,6 +25,9 @@ spec :: Spec
 spec = do
   decode_spec
 
+loadSample :: FilePath -> IO BSL.ByteString
+loadSample filename = BSL.readFile ("samples/data/" ++ filename)
+
 decode_spec :: Spec
 decode_spec = describe "decodeWith" $ do
   describe "authentication challenge" $ do
@@ -40,7 +45,41 @@ decode_spec = describe "decodeWith" $ do
                                       meta = mempty
                                     }
     forM_ ["v1", "v2", "v3"] $ \graphson_ver -> specify graphson_ver $ do
-      got <- decodeWith codec <$> BSL.readFile ("samples/data/response_auth_" ++ graphson_ver ++ ".json")
+      got <- decodeWith codec <$> loadSample ("response_auth_" ++ graphson_ver ++ ".json")
       got `shouldBe` Right exp_msg
-      
-    
+
+  describe "standard response" $ do
+    let codec :: Codec OpEval (GraphSON [GraphSON Value])
+        codec = jsonCodec
+        expMsg d = ResponseMessage { requestId = fromJust $ UUID.fromString "41d2e28a-20a4-4ab0-b379-d810dede3786",
+                                     status = exp_status,
+                                     result = expResult d
+                                   }
+        exp_status = ResponseStatus { code = Success,
+                                      message = "",
+                                      attributes = mempty
+                                    }
+        expResult d = ResponseResult { resultData = d,
+                                       meta = mempty
+                                     }
+        exp_v1 = A.object
+                 [ "id" .= A.Number 1,
+                   "label" .= A.String "person",
+                   "type" .= A.String "vertex"
+                 ]
+        exp_v2 = A.object
+                 [ "id" .= A.object ["@type" .= A.String "g:Int32", "@value" .= A.Number 1],
+                   "label" .= A.String "person"
+                 ]
+        exp_v3 = exp_v2
+        sampleFile v = "response_standard_" ++ v ++ ".json"
+    specify "v1" $ do
+      got <- decodeWith codec <$> loadSample (sampleFile "v1")
+      got `shouldBe` Right (expMsg $ nonTypedGraphSON [nonTypedGraphSON exp_v1])
+    specify "v2" $ do
+      got <- decodeWith codec <$> loadSample (sampleFile "v2")
+      got `shouldBe` Right (expMsg $ nonTypedGraphSON [typedGraphSON' "g:Vertex" exp_v2])
+    specify "v3" $ do
+      got <- decodeWith codec <$> loadSample (sampleFile "v3")
+      got `shouldBe` Right (expMsg $ typedGraphSON [typedGraphSON' "g:Vertex" exp_v3])
+
