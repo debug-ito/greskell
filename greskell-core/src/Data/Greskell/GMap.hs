c8b13426b4629a15a505bea0774d9f3c571e1f30
Toshio Ito <debug.ito@gmail.com>
2018-04-16 18:57:56 +0900

    add GMap. implementation not yet completed.

---
+++ greskell-core/src/Data/Greskell/GMap.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE TypeFamilies #-}
+-- |
+-- Module: Data.Greskell.GMap
+-- Description: data type for g:Map
+-- Maintainer: Toshio Ito <debug.ito@gmail.com>
+--
+-- 
+module Data.Greskell.GMap
+       (GMap(..)) where
+
+import Data.Aeson (FromJSON(..), ToJSON(..))
+import GHC.Exts (IsList(Item, fromList))
+
+-- $
+-- >>> import qualified Data.Aeson as Aeson
+-- >>> import Data.HashMap.Strict (HashMap, toList)
+-- >>> import Data.List (sort)
+
+-- | 'GMap' corresponds to @g:Map@ type in GraphSON.
+-- 
+-- In GraphSON v3, @g:Map@ is encoded as a flattened list of keys and
+-- values. 'FromJSON' and 'ToJSON' instances of 'GMap' implements this
+-- encoding.
+--
+-- - type @c@: container type for a map (e.g. 'Data.Map.Map' and
+--   'Data.HashMap.Strict.HashMap').
+-- - type @k@: key of the map.
+-- - type @v@: value of the map.
+--
+-- >>> fmap (sort . toList) $ (Aeson.decode "[10, \"ten\", 11, \"eleven\"]" :: Maybe (GMap HashMap Int String))
+-- Just [(10,"ten"),(11,"evel")]
+newtype GMap c k v = GMap { unGMap :: c k v }
+                   deriving (Show,Eq,Ord)
+
+instance (FromJSON k, FromJSON v, IsList (c k v), Item (c k v) ~ (k,v)) => FromJSON (GMap c k v) where
+  parseJSON = undefined
+
+instance (ToJSON k, ToJSON v, IsList (c k v), Item (c k v) ~ (k,v)) => ToJSON (GMap c k v) where
+  toJSON = undefined
+
+
