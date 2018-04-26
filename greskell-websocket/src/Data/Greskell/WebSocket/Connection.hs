0cd70171cb19b026be8a27e99bc3e619dd04de08
Toshio Ito <debug.ito@gmail.com>
2018-04-26 21:27:50 +0900

    Connection: implementing.. still a lot of todos.

--- greskell-websocket/src/Data/Greskell/WebSocket/Connection.hs
+++ greskell-websocket/src/Data/Greskell/WebSocket/Connection.hs
@@ -19,22 +19,21 @@ module Data.Greskell.WebSocket.Connection
        ) where
 
 import Control.Applicative ((<$>), (<|>))
-import Control.Concurrent.Async (withAsync)
+import Control.Concurrent.Async (withAsync, Async, async)
 import Control.Concurrent.STM
-  ( TBQueue, readTBQueue,
+  ( TBQueue, readTBQueue, newTBQueueIO,
     TQueue, writeTQueue, newTQueueIO, readTQueue,
     atomically
   )
+import Data.Aeson (Value)
 import Data.UUID (UUID)
 import qualified Network.WebSockets as WS
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.HashTable.IO as HT
 
 import Data.Greskell.WebSocket.Codec (Codec(decodeWith, encodeWith))
 import Data.Greskell.WebSocket.Request (RequestMessage)
-import Data.Greskell.WebSocket.Response (ResponseMessage)
-
--- | A WebSocket connection to a Gremlin Server.
-data Connection
+import Data.Greskell.WebSocket.Response (ResponseMessage(ResponseMessage, requestId))
 
 -- | Host name or an IP address.
 type Host = String
@@ -48,12 +47,19 @@ type Path = String
 -- | Make a 'Connection' to a Gremlin Server.
 --
 -- TODO: define exception spec.
-connect :: Host -> Port -> Path -> IO Connection
-connect = undefined
+connect :: Codec s -> Host -> Port -> Path -> IO (Connection s)
+connect codec host port path = do
+  qreq <- newTBQueueIO qreq_size
+  ws_thread <- async $ runWSConn codec host port path qreq
+  return $ Connection { connQReq = qreq,
+                        connWSThread = ws_thread
+                      }
+  where
+    qreq_size = 512 -- TODO: make it configurable
 
 -- | Close the 'Connection'.
-close :: Connection -> IO ()
-close = undefined
+close :: Connection s -> IO ()
+close = undefined -- TODO
 
 
 type RawReq = BSL.ByteString
@@ -62,27 +68,36 @@ type RawRes = BSL.ByteString
 
 type ReqID = UUID
 
-data ReqPack = ReqPack
-               { reqData :: RawReq,
-                 reqId :: ReqID,
-                 reqOutput :: TQueue RawRes
-               }
+-- | Package of request data and related stuff. It's passed from the
+-- caller thread into WS handling thread.
+data ReqPack s = ReqPack
+                 { reqData :: RawReq,
+                   reqId :: ReqID,
+                   reqOutput :: TQueue (ResponseMessage s)
+                 }
 
-startWS :: Host -> Port -> Path -> TBQueue ReqPack -> IO ()
-startWS host port path qreq = WS.runClient host port path $ \wsconn -> do
+-- | A WebSocket connection to a Gremlin Server.
+data Connection s =
+  Connection
+  { connQReq :: TBQueue (ReqPack s),
+    connWSThread :: Async ()
+  }
+
+
+-- | A thread taking care of a WS connection.
+runWSConn :: Codec s -> Host -> Port -> Path -> TBQueue (ReqPack s) -> IO ()
+runWSConn codec host port path qreq = WS.runClient host port path $ \wsconn -> do -- TODO: handle exception
   qres <- newTQueueIO
+  req_pool <- HT.new
   withAsync (runRxLoop wsconn qres) $ \_ -> 
-    runMuxLoop (WSContext wsconn) qreq qres 
+    runMuxLoop wsconn req_pool codec qreq qres 
 
+-- | (requestId of pending request) --> (output channel of the corresponding responses)
+type ReqPool s = HT.BasicHashTable ReqID (TQueue (ResponseMessage s))
 
-data WSContext s = WSContext
-                   { wscConn :: WS.Connection
-                     -- TODO: Map requestId (TQueue BS)的なやつを入れる。IORefかな？ mutable特化のものが望ましい。
-                   }
-
--- | Multiplexer thread.
-runMuxLoop :: WSContext s -> TBQueue ReqPack -> TQueue RawRes -> IO ()
-runMuxLoop (WSContext { wscConn = wsconn }) qreq qres = loop
+-- | Multiplexer loop.
+runMuxLoop :: WS.Connection -> ReqPool s -> Codec s -> TBQueue (ReqPack s) -> TQueue RawRes -> IO ()
+runMuxLoop wsconn req_pool codec qreq qres = loop
   where
     loop = do
       event <- atomically $ (Left <$> readTBQueue qreq) <|> (Right <$> readTQueue qres)
@@ -91,9 +106,18 @@ runMuxLoop (WSContext { wscConn = wsconn }) qreq qres = loop
        Right res -> handleRes res
       loop
     handleReq req = do
-      -- TODO: handle new request. Who should create a new requestId?
+      res_output <- newTQueueIO
+      HT.insert req_pool (reqId req) res_output
       WS.sendBinaryData wsconn $ reqData req
-    handleRes res = undefined -- TODO: handle response message
+    handleRes res = case decodeWith codec res of -- TODO: perhaps we have to decode MIME type packaging
+      Left err -> undefined -- TODO: handle parse error
+      Right res_msg -> handleResMsg res_msg
+    handleResMsg res_msg@(ResponseMessage { requestId = rid }) = do
+      m_qout <- HT.lookup req_pool rid
+      case m_qout of
+       Nothing -> undefined -- TODO: handle unknown requestId case.
+       Just qout -> atomically $ writeTQueue qout res_msg
+
 
 -- | Receiver thread.
 runRxLoop :: WS.Connection -> TQueue RawRes -> IO ()
@@ -112,7 +136,7 @@ data ResponseHandle s
 -- | Send a 'RequestMessage' to the server.
 --
 -- TODO: define exception spec.
-sendRequest :: Codec s -> Connection -> RequestMessage -> IO (ResponseHandle s)
+sendRequest :: Connection s -> RequestMessage -> IO (ResponseHandle s)
 sendRequest = undefined
 
 -- | Get a 'ResponseMessage' from 'ResponseHandle'.
