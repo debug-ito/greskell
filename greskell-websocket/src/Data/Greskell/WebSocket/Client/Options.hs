7d3799049fdc1935fb6139584c9208368fc0f5fa
Toshio Ito <debug.ito@gmail.com>
2018-05-10 21:25:52 +0900

    Client.Options: reorganize export lists.

--- greskell-websocket/src/Data/Greskell/WebSocket/Client/Options.hs
+++ greskell-websocket/src/Data/Greskell/WebSocket/Client/Options.hs
@@ -5,14 +5,33 @@
 --
 -- 
 module Data.Greskell.WebSocket.Client.Options
-       ( Options(..)
+       ( -- * Options
+         Options,
+         defOptions,
+         -- ** accessor functions
+         connectionSettings,
+         -- * Settings
+         module Data.Greskell.WebSocket.Connection.Settings
        ) where
 
 import Data.Aeson (Value)
 
-import Data.Greskell.WebSocket.Connection.Settings (Settings)
+import Data.Greskell.WebSocket.Connection (Connection)
+import Data.Greskell.WebSocket.Connection.Settings
 
+-- | Configuration options to create a client for Gremlin Server.
+--
+-- You can get the default 'Options' by 'defOptions' function, and
+-- customize its fields by accessor functions.
 data Options =
   Options
   { connectionSettings :: Settings Value
+    -- ^ Settings for the underlying 'Connection'. Default:
+    -- 'defJSONSettings'.
+  }
+
+defOptions :: Options
+defOptions =
+  Options
+  { connectionSettings = defJSONSettings
   }
