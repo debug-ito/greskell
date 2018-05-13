4d28a58dd1fdd102db78a25281bb302b079891ac
Toshio Ito <debug.ito@gmail.com>
2018-05-13 14:15:08 +0900

    IteratorItem: add comments and docs.

--- greskell-core/src/Data/Greskell/IteratorItem.hs
+++ greskell-core/src/Data/Greskell/IteratorItem.hs
@@ -20,6 +20,19 @@ import Data.Greskell.GMap (GMap, GMapEntry)
 -- In Gremlin, the conversion is done by
 -- @org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils.asIterator@
 -- method.
+--
+-- Conversion rule of thumb:
+--
+-- - @Iterator@ and @Iterable@ types like @List@, @Stream@ and
+--   @GraphTraversal@ are converted to their element types.
+-- - @Map@ type is converted to its @Map.Entry@.
+-- - Other types are converted to themselves.
+--
+-- Caveat:
+--
+-- - Because Haskell's 'String' is '[Char]', @IteratorItem String@
+--   returns 'Char', which is incorrect. Use 'Text' if you want to
+--   deal with @String@s in Gremlin.
 type family IteratorItem a
 
 type instance IteratorItem Int = Int
@@ -33,13 +46,14 @@ type instance IteratorItem (GMap k v) = GMapEntry k v
 
 type instance IteratorItem (HashMap k v) = GMapEntry k v
 
+-- About encoding of Map.Entry
+--
+-- It seems that GraphSON encodes a Map.Entry as if it were a
+-- single-entry Map, but who is responsble for that? jackson?
+--
+-- Maybe these topics are related?
+--
+-- - https://github.com/fasterxml/jackson-databind/issues/565
+-- - https://fasterxml.github.io/jackson-databind/javadoc/2.8/com/fasterxml/jackson/databind/ser/impl/MapEntrySerializer.html
+
 -- TODO: add more type instances (mostly for scalar types)
-
--- Map -> Iterator<Map.Entry>なんだけど、Map.Entryは1要素Mapとしてエンコードされて返されるようだ。
--- しかしこのエンコードをやってるやつは誰だ？
--- jacksonがやってるのかな。
--- https://github.com/fasterxml/jackson-databind/issues/565
--- これか？ でも時期が近すぎるような
--- https://fasterxml.github.io/jackson-databind/javadoc/2.8/com/fasterxml/jackson/databind/ser/impl/MapEntrySerializer.html
-
--- ま、いいか。
