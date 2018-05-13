587e74b9c3ed55754aaea299d6a901310231f1f9
Toshio Ito <debug.ito@gmail.com>
2018-05-13 13:46:41 +0900

    ServerTest.Client: add test cases for lists and maps.

--- greskell-websocket/test/ServerTest/Client.hs
+++ greskell-websocket/test/ServerTest/Client.hs
@@ -1,15 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 module ServerTest.Client (main,spec) where
 
 import Control.Exception.Safe (bracket)
+import Control.Monad (forM_)
 import Test.Hspec
 
 import Data.Greskell.Greskell (Greskell)
+import qualified Data.Greskell.Greskell as G
+import Data.Greskell.GMap (GMap, GMapEntry, unGMapEntry)
 
 import Data.Greskell.WebSocket.Client
   ( Host, Port, Client, Options,
     connectWith, close, submit,
     defOptions,
-    nextResult
+    nextResult, slurpResults
   )
 
 import TestUtil.Env (withEnvForExtServer)
@@ -36,3 +40,16 @@ client_basic_spec = do
     nextResult rh `shouldReturn` Just 108
     nextResult rh `shouldReturn` Nothing
     nextResult rh `shouldReturn` Nothing
+  specify "eval [Int]" $ withClient $ \client -> do
+    let g = G.list $ map fromInteger [1..20] :: Greskell [Int]
+    rh <- submit client g Nothing
+    forM_  [1..20] $ \n -> 
+      nextResult rh `shouldReturn` Just n
+    nextResult rh `shouldReturn` Nothing
+    nextResult rh `shouldReturn` Nothing
+  specify "eval (GMap Int String)" $ withClient $ \client -> do
+    let g :: Greskell (GMap Int String)
+        g = G.unsafeGreskell "[100: 'hoge', 200: 'foo', 300: 'bar']"
+    rh <- submit client g Nothing
+    got <- fmap (map unGMapEntry) $ slurpResults rh
+    got `shouldMatchList` [(100, "hoge"), (200, "foo"), (300, "bar")]
