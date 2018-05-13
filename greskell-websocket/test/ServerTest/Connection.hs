3611bb41e95c7de1a27d4ba3db1bb9ec5293b516
Toshio Ito <debug.ito@gmail.com>
2018-05-13 15:55:25 +0900

    refactor into TestUtil.MockServer

--- greskell-websocket/test/ServerTest/Connection.hs
+++ greskell-websocket/test/ServerTest/Connection.hs
@@ -26,7 +26,6 @@ import qualified Network.WebSockets as WS
 import System.IO (stderr, hPutStrLn)
 import Test.Hspec
 
-import Data.Greskell.WebSocket.Codec (decodeBinary)
 import Data.Greskell.WebSocket.Connection
   ( Host, Port, Connection, ResponseHandle,
     close, connect, sendRequest', sendRequest, slurpResponses,
@@ -47,6 +46,7 @@ import qualified Data.Greskell.WebSocket.Response as Response
 
 import qualified TestUtil.TCounter as TCounter
 import TestUtil.Env (withEnvForExtServer, withEnvForIntServer)
+import TestUtil.MockServer (wsServer, receiveRequest, simpleRawResponse)
 
 
 main :: IO ()
@@ -252,30 +252,6 @@ conn_close_spec = describe "close" $ do
     nextResponse rh `shouldThrow` expEx 
     nextResponse rh `shouldThrow` expEx
   
-
-wsServer :: Int -- ^ port number
-         -> (WS.Connection -> IO ())
-         -> IO ()
-wsServer p act = WS.runServer "localhost" p $ \pending_conn ->
-  act =<< WS.acceptRequest pending_conn
-
-parseRequest :: BSL.ByteString -> Either String RequestMessage
-parseRequest raw_msg = do
-  (_, payload) <- decodeBinary raw_msg
-  Aeson.eitherDecode payload
-
-receiveRequest :: WS.Connection -> IO RequestMessage
-receiveRequest wsconn = do
-  raw_msg <- WS.receiveData wsconn
-  case parseRequest raw_msg of
-   Left e -> throwString e
-   Right r -> return r
-
-simpleRawResponse :: UUID -> Int -> Text -> Text
-simpleRawResponse request_id status_code data_content =
-  "{\"requestId\":\"" <> UUID.toText request_id <> "\","
-  <> "\"status\":{\"code\":" <> (pack $ show status_code) <> ",\"message\":\"\",\"attributes\":{}},"
-  <> "\"result\":{\"data\":" <> data_content <> ",\"meta\":{}}}"
 
 succUUID :: UUID -> UUID
 succUUID orig = UUID.fromWords a b c d'
---
+++ greskell-websocket/test/TestUtil/MockServer.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+module TestUtil.MockServer
+       ( wsServer,
+         parseRequest,
+         receiveRequest,
+         simpleRawResponse
+       ) where
+
+import Control.Exception.Safe (throwString)
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as BSL
+import Data.Monoid ((<>))
+import Data.Text (Text, pack)
+import Data.UUID (UUID)
+import qualified Data.UUID as UUID
+import qualified Network.WebSockets as WS
+
+import Data.Greskell.WebSocket.Codec (decodeBinary)
+import Data.Greskell.WebSocket.Request (RequestMessage)
+
+
+wsServer :: Int -- ^ port number
+         -> (WS.Connection -> IO ())
+         -> IO ()
+wsServer p act = WS.runServer "localhost" p $ \pending_conn ->
+  act =<< WS.acceptRequest pending_conn
+
+parseRequest :: BSL.ByteString -> Either String RequestMessage
+parseRequest raw_msg = do
+  (_, payload) <- decodeBinary raw_msg
+  Aeson.eitherDecode payload
+
+receiveRequest :: WS.Connection -> IO RequestMessage
+receiveRequest wsconn = do
+  raw_msg <- WS.receiveData wsconn
+  case parseRequest raw_msg of
+   Left e -> throwString e
+   Right r -> return r
+
+simpleRawResponse :: UUID -> Int -> Text -> Text
+simpleRawResponse request_id status_code data_content =
+  "{\"requestId\":\"" <> UUID.toText request_id <> "\","
+  <> "\"status\":{\"code\":" <> (pack $ show status_code) <> ",\"message\":\"\",\"attributes\":{}},"
+  <> "\"result\":{\"data\":" <> data_content <> ",\"meta\":{}}}"
+
