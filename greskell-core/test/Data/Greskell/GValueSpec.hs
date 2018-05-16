893a8e77955aa2570c189b25df5f028d5421cf1c
Toshio Ito <debug.ito@gmail.com>
2018-05-16 21:43:02 +0900

    GValueSpec: add some test cases.

--- greskell-core/test/Data/Greskell/GValueSpec.hs
+++ greskell-core/test/Data/Greskell/GValueSpec.hs
@@ -4,8 +4,10 @@ module Data.Greskell.GValueSpec (main,spec) where
 import Data.Aeson (FromJSON)
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.HashMap.Strict as HM
 import Data.Monoid ((<>))
 import Data.Text (Text)
+import qualified Data.Vector as V
 import Test.Hspec
 
 import Data.Greskell.GraphSON (nonTypedGraphSON, typedGraphSON')
@@ -25,7 +27,53 @@ spec = do
     fromToJSON "wrapped bool" (gson "g:Boolean" "true") (wrapped "g:Boolean" $ GBool True)
     fromToJSON "bare str" "\"hoge\"" (bare $ GString "hoge")
     fromToJSON "wrapped str" (gson "g:String" "\"hoge\"") (wrapped "g:String" $ GString "hoge")
-    -- TODO: array, object and nested values.
+    fromToJSON "bare array" "[null, \"foo\", 100]"
+      (bare $ GArray $ fmap bare $ V.fromList [GNull, GString "foo", GNumber 100])
+    fromToJSON "wrapped array" (gson "g:List" "[null, \"foo\", 100]")
+      (wrapped "g:List" $ GArray $ fmap bare $ V.fromList [GNull, GString "foo", GNumber 100])
+    fromToJSON "bare object" "{\"foo\": \"bar\", \"hoge\": 99, \"quux\": null}"
+      (bare $ GObject $ fmap bare $ HM.fromList [("foo", GString "bar"), ("hoge", GNumber 99), ("quux", GNull)])
+    fromToJSON "wrapped object" (gson "g:Map" "{\"foo\": \"bar\", \"hoge\": 99, \"quux\": null}")
+      (wrapped "g:Map" $ GObject $ fmap bare $ HM.fromList [("foo", GString "bar"), ("hoge", GNumber 99), ("quux", GNull)])
+    nested_spec
+    double_wrap_spec
+
+nested_spec :: Spec
+nested_spec = fromToJSON "mixed nested" input expected
+  where
+    input = gson "g:List" ("[100, " <> elem_a <> ", " <> elem_b <> "]")
+    elem_a = gson "g:Map" ("{\"foo\": 100, \"bar\":" <> (gson "g:Int" "200") <> "}")
+    elem_b = gson "g:List" ("[null, " <> (gson "g:Object" "null") <> ", " <> elem_c <> ", " <> (gson "g:Boolean" "false") <> "]")
+    elem_c = "{\"xxx\": " <> (gson "g:Int" "200") <> ", \"yyy\": true}"
+    expected = wrapped "g:List" $ GArray $ V.fromList [ bare $ GNumber 100, exp_a, exp_b ]
+    exp_a = wrapped "g:Map" $ GObject $ HM.fromList
+            [ ("foo", bare $ GNumber 100),
+              ("bar", wrapped "g:Int" $ GNumber 200)
+            ]
+    exp_b = wrapped "g:List" $ GArray $ V.fromList
+            [ bare GNull,
+              wrapped "g:Object" GNull,
+              exp_c,
+              wrapped "g:Boolean" $ GBool False
+            ]
+    exp_c = bare $ GObject $ HM.fromList
+            [ ("xxx", wrapped "g:Int" $ GNumber 200),
+              ("yyy", bare $ GBool True)
+            ]
+
+-- Basically this case should not appear in GraphSON encoding, but
+-- GValue can parse it. However, the parsed GValue has a GObject with
+-- "@type" and "@value" keys explicitly.
+double_wrap_spec :: Spec
+double_wrap_spec = fromToJSON "double wrapped" input expected
+  where
+    input = gson "g:Object" $ gson "g:Int" "100"
+    expected = wrapped "g:Object" $ GObject $ HM.fromList
+               [ ("@type", bare $ GString "g:Int"),
+                 ("@value", bare $ GNumber 100)
+               ]
+
+    -- TODO: decode error case.
 
 forceDecode :: FromJSON a => BSL.ByteString -> a
 forceDecode json = case Aeson.eitherDecode json of
