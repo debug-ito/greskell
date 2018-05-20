6954a46b3f39e447cbace03e2814c46ba1a0187e
Toshio Ito <debug.ito@gmail.com>
2018-05-20 11:51:46 +0900

    GraphSONTyped: add instances to types fron containers package.

--- greskell-core/src/Data/Greskell/GraphSON/GraphSONTyped.hs
+++ greskell-core/src/Data/Greskell/GraphSON/GraphSONTyped.hs
@@ -12,10 +12,19 @@ module Data.Greskell.GraphSON.GraphSONTyped
 
 import Data.Text (Text)
 import Data.Vector (Vector)
-import qualified Data.HashMap.Lazy as HML
+import qualified Data.HashMap.Lazy as L (HashMap)
+import qualified Data.HashMap.Strict as S (HashMap)
 import Data.HashSet (HashSet)
-import Data.Scientific (Scientific)
 import Data.Int (Int8, Int16, Int32, Int64)
+import qualified Data.IntMap.Lazy as L (IntMap)
+import qualified Data.IntMap.Strict as S (IntMap)
+import Data.IntSet (IntSet)
+import qualified Data.Map.Lazy as L (Map)
+import qualified Data.Map.Strict as S (Map)
+import Data.Scientific (Scientific)
+import Data.Sequence (Seq)
+import Data.Set (Set)
+
 
 -- | Types that have an intrinsic type ID for 'gsonType' field.
 class GraphSONTyped a where
@@ -50,16 +59,42 @@ instance GraphSONTyped [a] where
 instance GraphSONTyped (Vector a) where
   gsonTypeFor _ = "g:List"
 
+instance GraphSONTyped (Seq a) where
+  gsonTypeFor _ = "g:List"
+
 -- | Map to \"g:Double\".
 instance GraphSONTyped Scientific where
   gsonTypeFor _ = "g:Double"
 
--- | Note that Lazy HashMap and Strict HashMap are the same data type.
-instance GraphSONTyped (HML.HashMap k v) where
-  gsonTypeFor _ = "g:Map"
-
 instance GraphSONTyped (HashSet a) where
   gsonTypeFor _ = "g:Set"
+
+instance GraphSONTyped IntSet where
+  gsonTypeFor _ = "g:Set"
+
+instance GraphSONTyped (Set a) where
+  gsonTypeFor _ = "g:Set"
+
+instance GraphSONTyped (L.HashMap k v) where
+  gsonTypeFor _ = "g:Map"
+
+instance GraphSONTyped (L.Map k v) where
+  gsonTypeFor _= "g:Map"
+
+instance GraphSONTyped (L.IntMap v) where
+  gsonTypeFor _= "g:Map"
+
+-- -- Implementation of Lazy and Strict types are the same.
+-- 
+-- instance GraphSONTyped (S.HashMap k v) where
+--   gsonTypeFor _ = "g:Map"
+-- 
+-- instance GraphSONTyped (S.Map k v) where
+--   gsonTypeFor _= "g:Map"
+-- 
+-- instance GraphSONTyped (S.IntMap v) where
+--   gsonTypeFor _= "g:Map"
+
 
 instance (GraphSONTyped a, GraphSONTyped b) => GraphSONTyped (Either a b) where
   gsonTypeFor e = either gsonTypeFor gsonTypeFor e
