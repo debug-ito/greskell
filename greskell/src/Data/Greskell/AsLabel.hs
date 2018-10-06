622bb07f681fe9938f49c96914c3a64205cd7d7b
Toshio Ito <debug.ito@gmail.com>
2018-10-06 19:02:22 +0900

    add SelectedMap with some TODOs.

--- greskell/src/Data/Greskell/AsLabel.hs
+++ greskell/src/Data/Greskell/AsLabel.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 -- |
 -- Module: Data.Greskell.AsLabel
 -- Description: Label string used in .as step
@@ -6,8 +6,17 @@
 --
 -- 
 module Data.Greskell.AsLabel
-       (AsLabel(..)) where
+       ( AsLabel(..),
+         SelectedMap,
+         lookupAs,
+         lookupAsM,
+         AsLookupException(..)
+       ) where
 
+import Control.Exception (Exception)
+import Control.Monad.Catch (MonadThrow(..))
+import Data.HashMap.Strict (HashMap)
+import Data.Greskell.GraphSON (GValue, GraphSONTyped(..), FromGraphSON(..))
 import Data.Greskell.Greskell (ToGreskell(..))
 import qualified Data.Greskell.Greskell as Greskell
 import Data.Text (Text)
@@ -22,3 +31,24 @@ instance ToGreskell (AsLabel a) where
   type GreskellReturn (AsLabel a) = Text
   toGreskell (AsLabel t) = Greskell.string t
 
+-- | A value-heterogeneous map keyed with 'AsLabel'. Obtained from
+-- @.select@ step, for example.
+newtype SelectedMap = SelectedMap (HashMap Text GValue)
+                    deriving (Show,Eq)
+
+instance GraphSONTyped SelectedMap where
+  gsonTypeFor _ = "g:Map"
+
+instance FromGraphSON SelectedMap where
+  parseGraphSON gv = fmap SelectedMap $ parseGraphSON gv
+
+data AsLookupException = AsLookupException -- TODO: write proper options
+                       deriving (Show,Eq,Ord)
+
+instance Exception AsLookupException
+
+lookupAs :: AsLabel a -> SelectedMap -> Either AsLookupException a
+lookupAs = undefined -- TODO
+
+lookupAsM :: MonadThrow m => AsLabel a -> SelectedMap -> m a
+lookupAsM l m = either throwM return $ lookupAs l m
