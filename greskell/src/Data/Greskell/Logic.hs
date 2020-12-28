03a6ab1b4ad139519adce490aa9f3d492a93dd3a
Toshio Ito <debug.ito@gmail.com>
2020-12-28 22:00:33 +0900

    Logic: make the developer note public in the doc.

--- greskell/src/Data/Greskell/Logic.hs
+++ greskell/src/Data/Greskell/Logic.hs
@@ -4,6 +4,28 @@
 -- Description: Logic tree data structure
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 --
+-- Developer note: This module defines 'Logic', a data structure for
+-- logic operation tree. There are already similar packages to this
+-- module, but, none of them satisfy our requirements.
+--
+-- Boolean/logic tree data structures
+--
+-- - https://hackage.haskell.org/package/boolean-normal-forms
+-- - https://hackage.haskell.org/package/logic-classes
+-- - https://hackage.haskell.org/package/PropLogic
+-- - https://hackage.haskell.org/package/Logic
+-- - https://hackage.haskell.org/package/boolean-like
+--
+-- Typeclasses about boolean/logic operations
+--
+-- - https://hackage.haskell.org/package/Boolean
+-- - https://hackage.haskell.org/package/cond
+--
+-- Trees that contain heterogeneous values
+--
+-- - http://hackage.haskell.org/package/dual-tree
+-- - http://hackage.haskell.org/package/fingertree
+--
 -- @since 1.2.0.0 
 module Data.Greskell.Logic
   ( Logic(..),
@@ -25,26 +47,6 @@ data Logic a =
   | Or  (Logic a) [Logic a] -- ^ \"or\" logic operator
   | Not (Logic a) -- ^ \"not\" logic operator
   deriving (Show,Eq,Ord,Generic)
-
--- Developer note: There are already similar packages to this
--- module. However, none of them satisfy our requirements, I think.
---
--- - Boolean/logic tree data structures
---   - https://hackage.haskell.org/package/boolean-normal-forms
---   - https://hackage.haskell.org/package/logic-classes-1.5.3
---   - https://hackage.haskell.org/package/PropLogic
---   - https://hackage.haskell.org/package/Logic
---   - https://hackage.haskell.org/package/boolean-like
---
--- - Typeclasses about boolean/logic operations
---   - https://hackage.haskell.org/package/Boolean
---   - https://hackage.haskell.org/package/cond
---
--- - Trees that contain heterogeneous values
---   - http://hackage.haskell.org/package/dual-tree
---   - http://hackage.haskell.org/package/fingertree
-
-
 
 instance Functor Logic where
   fmap f l =
