2e2bf17c36febf73e8be384d09b53f78c4eed629
Toshio Ito <debug.ito@gmail.com>
2021-12-14 10:22:55 +0900

    try writing the Typecheck test.

--- greskell/test/Typecheck.hs
+++ greskell/test/Typecheck.hs
@@ -1,10 +1,11 @@
 {-# OPTIONS_GHC -fdefer-type-errors #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main,spec) where
 
 import Test.Hspec
 import Test.ShouldNotTypecheck (shouldNotTypecheck)
 
-import Data.Greskell.GTraversal (Walk)
+import Data.Greskell.GTraversal (Walk, WalkType, Filter, Transform, SideEffect, gFilter, unsafeWalk)
 
 main :: IO ()
 main = hspec spec
@@ -12,5 +13,9 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Split typeclass" $ do
-    True `shouldBe` False
-    
+    specify "SideEffect -> Filter" $ do
+      putStrLn $ show ((gFilter :: Walk SideEffect Int Int -> Walk Filter Int Int) anyWalk)
+
+
+anyWalk :: WalkType c => Walk c Int Int
+anyWalk = unsafeWalk "foo" []
