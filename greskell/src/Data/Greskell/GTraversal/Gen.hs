07b63c772747f9bb99d23311fdd32497ed9edd22
Toshio Ito <debug.ito@gmail.com>
2023-02-23 21:27:53 +0900

    start writing GTraversal.Gen.

---
+++ greskell/src/Data/Greskell/GTraversal/Gen.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE FlexibleContexts #-}
+-- | This module has Gremlin traversals defined in 'Data.Greskell.GTraversal' generalized on the
+-- walk type.
+module Data.Greskell.GTraversal.Gen
+    ( -- * GraphTraversalSource
+      sV
+    , sV'
+--     , sE
+--     , sE'
+--     , sAddV
+--     , sAddV'
+--       -- ** Filter steps
+--     , gIdentity
+--     , gIdentity'
+--     , gFilter
+--     , gCyclicPath
+--     , gCyclicPath'
+--     , gSimplePath
+--     , gSimplePath'
+--       -- ** Is step
+--     , gIs
+--     , gIs'
+--     , gIsP
+--     , gIsP'
+--       -- ** Has steps
+--     , gHas1
+--     , gHas1'
+--     , gHas2
+--     , gHas2'
+--     , gHas2P
+--     , gHas2P'
+--     , gHasLabel
+--     , gHasLabel'
+--     , gHasLabelP
+--     , gHasLabelP'
+--     , gHasId
+--     , gHasId'
+--     , gHasIdP
+--     , gHasIdP'
+--     , gHasKey
+--     , gHasKey'
+--     , gHasKeyP
+--     , gHasKeyP'
+--     , gHasValue
+--     , gHasValue'
+--     , gHasValueP
+--     , gHasValueP'
+--       -- ** Logic steps
+--     , gAnd
+--     , gOr
+--     , gNot
+--       -- ** Where step
+--     , gWhereP1
+--     , gWhereP1'
+--     , gWhereP2
+--     , gWhereP2'
+--       -- ** Sorting steps
+--     , gOrder
+--       -- ** Paging steps
+--     , gRange
+--     , gLimit
+--     , gTail
+--     , gSkip
+--       -- ** Repeat step
+--     , gRepeat
+--     , gTimes
+--     , gUntilHead
+--     , gUntilTail
+--     , gEmitHead
+--     , gEmitTail
+--     , gEmitHeadT
+--     , gEmitTailT
+--     , gLoops
+--     , RepeatUntil (..)
+--     , RepeatEmit (..)
+--     , RepeatPos (..)
+--     , RepeatLabel (..)
+--       -- ** Branching steps
+--     , gLocal
+--     , gUnion
+--     , gCoalesce
+--     , gChoose3
+--       -- ** Barrier steps
+--     , gBarrier
+--     , gDedup
+--     , gDedupN
+--       -- ** Transformation steps
+--     , gFlatMap
+--     , gFlatMap'
+--     , gV
+--     , gV'
+--     , gConstant
+--     , gProject
+--       -- ** As step
+--     , gAs
+--       -- ** Accessor steps
+--     , gValues
+--     , gProperties
+--     , gId
+--     , gLabel
+--     , gValueMap
+--     , gElementMap
+--     , gSelect1
+--     , gSelectN
+--     , gSelectBy1
+--     , gSelectByN
+--     , gUnfold
+--     , gPath
+--     , gPathBy
+--       -- ** Summarizing steps
+--     , gFold
+--     , gCount
+--       -- ** Graph traversal steps
+--     , gOut
+--     , gOut'
+--     , gOutE
+--     , gOutE'
+--     , gOutV
+--     , gOutV'
+--     , gIn
+--     , gIn'
+--     , gInE
+--     , gInE'
+--     , gInV
+--     , gInV'
+--       -- ** Match step
+--     , gMatch
+--     , MatchPattern (..)
+--     , mPattern
+--     , MatchResult
+--       -- ** Side-effect steps
+--     , gSideEffect
+--     , gSideEffect'
+--       -- ** Graph manipulation steps
+--     , gAddV
+--     , gAddV'
+--     , gAddE
+--     , gAddE'
+--     , AddAnchor
+--     , gFrom
+--     , gTo
+--     , gDrop
+--     , gDropP
+--     , gProperty
+--     , gPropertyV
+    ) where
+
+import           Data.Greskell.Graph      (AVertex, ElementID, Vertex)
+import           Data.Greskell.Greskell   (Greskell)
+import           Data.Greskell.GTraversal (GTraversal, GraphTraversalSource, Lift,
+                                           ToGTraversal (..), Transform, WalkType)
+import qualified Data.Greskell.GTraversal as G
+
+sV :: (Vertex v, WalkType c, Lift Transform c) => [Greskell (ElementID v)] -> Greskell GraphTraversalSource -> GTraversal c () v
+sV ids src = liftWalk $ G.sV ids src
+
+sV' :: (WalkType c, Lift Transform c) => [Greskell (ElementID AVertex)] -> Greskell GraphTraversalSource -> GTraversal c () AVertex
+sV' = sV
+
